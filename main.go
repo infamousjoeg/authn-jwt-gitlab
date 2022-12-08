6633a55f9ea487a308db2a42ca318a1f695ca51f
Joe Garcia <joe@joe-garcia.com>
2022-12-08 12:43:44 -0500

    Print secret value to stdout

--- main.go
+++ main.go
@@ -10,66 +10,58 @@ import (
 
 // Environment variables to define:
 // CONJUR_APPLIANCE_URL, CONJUR_ACCOUNT, CONJUR_AUTHN_JWT_SERVICE_ID,
-// CONJUR_AUTHN_JWT_HOST_ID, CONJUR_AUTHN_JWT_TOKEN, CONJUR_USER_OBJECT,
-// CONJUR_PASS_OBJECT
+// CONJUR_AUTHN_JWT_TOKEN, CONJUR_SECRET_ID
+
+func checkEnvironmentVariables() error {
+	// Check for environment variables and error if one is missing.
+	variables := []string{
+		"CONJUR_APPLIANCE_URL",
+		"CONJUR_ACCOUNT",
+		"CONJUR_AUTHN_JWT_SERVICE_ID",
+		"CONJUR_AUTHN_JWT_TOKEN",
+		"CONJUR_SECRET_ID",
+	}
+
+	for _, variable := range variables {
+		if os.Getenv(variable) == "" {
+			return fmt.Errorf("Environment variable %s is not set", variable)
+		}
+	}
+
+	return nil
+}
 
 func main() {
 
 	// Check for environment variables and error if one is missing.
-	if os.Getenv("CONJUR_APPLIANCE_URL") == "" || os.Getenv("CONJUR_ACCOUNT") == "" {
-		log.Fatalf("Both CONJUR_APPLIANCE_URL and CONJUR_ACCOUNT environment variables must be set.")
+	err := checkEnvironmentVariables()
+	if err != nil {
+		log.Fatalf("%v", err)
 	}
 
-	//Defining Username & Password objects to retrieve, as per 12 factor
-	//this is being accomplished via env variables.
-	variableIdentifier := os.Getenv("CONJUR_PASS_OBJECT")
-	variableuserIdentifier := os.Getenv("CONJUR_USER_OBJECT")
+	// Defining secret ID to retrieve, as per 12 factor
+	// this is being accomplished via env variables.
+	variableIdentifier := os.Getenv("CONJUR_SECRET_ID")
 
-	//Loading configuration via defined Env vars:
-	//CONJUR_APPLIANCE, CONJUR_VERSION, CONJUR_ACCOUNT
+	// Loading configuration via defined Env vars:
+	// CONJUR_APPLIANCE & CONJUR_ACCOUNT
 	config, err := conjurapi.LoadConfig()
 	if err != nil {
 		log.Fatalf("Cannot load configuration. %s", err)
 	}
 
-	//Get Authorization token from shared store from sidecar
+	// Create a new Conjur client using environment variables
 	conjur, err := conjurapi.NewClientFromEnvironment(config)
 	if err != nil {
 		log.Fatalf("Cannot create new client from environment variables. %s", err)
 	}
 
-	if os.Getenv("Conjur") == "True" {
-
-		secretValue, err := conjur.RetrieveSecret(variableIdentifier)
-		if err != nil {
-			log.Fatalf("Could not retrieve secret. %s", err)
-		}
-
-		secretValueUser, err := conjur.RetrieveSecret(variableuserIdentifier)
-		if err != nil {
-			log.Fatalf("Could not retrieve secret. %s", err)
-		}
-
-		//Display Username & Password in log.
-		log.Printf("%s:%s", "The Username Used: ", secretValueUser)
-		log.Printf("%s:%s", "The Password Used: ", secretValue)
-
-	} else {
-
-		//Grab Password from Conjur
-		secretValue, err := conjur.RetrieveSecret(variableIdentifier)
-		if err != nil {
-			log.Fatalf("Could not retrieve secret. %s", err)
-		}
-
-		//Grab Username from Conjur
-		//secretValueUser, err := conjur.RetrieveSecret(variableuserIdentifier)
-		//if err != nil {
-		//	panic(err)
-		//}
-
-		//Display Username & Password in log.
-		fmt.Printf("%s", secretValue)
+	// Retrieve the secret value from Conjur
+	secretValue, err := conjur.RetrieveSecret(variableIdentifier)
+	if err != nil {
+		log.Fatalf("Cannot retrieve secret value for %s. %s", variableIdentifier, err)
 	}
 
+	// Print the secret value to stdout
+	fmt.Printf("%s", string(secretValue))
 }
